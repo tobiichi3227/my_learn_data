e7aac34872b78770782d96ec460bee2b61007919
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:58:37 +0800

    Create VPL13A.cpp

---
+++ tnfsh/VPL13A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+//#include <string> 
+//#include <string.h> //strlen
+//#include <sstream>
+//#include <iomanip>
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//#include <cmath>
+//#include <random>
+//#include <ctime>
+
+
+
+
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    //加速cin cout用
+    // fixed << setprecision(n) << variable
+    //取小數點用
+    
+    int n{}, sum{};
+    int i{0}, temp{};
+    while (cin >> n)
+    {
+	      i++;
+	      sum += n;
+	      if (sum >= 70000) 
+	      {
+	          cout << i << endl << sum << endl;
+	          break;
+	      }
+    }
+    //cout << sum << "\n";
+
+    return 0;
+} 
