56a4fb68ef00b7b89aaa07cf8244a1a7fef69870
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:20:20 +0800

    Create VPL7A.cpp

---
+++ tnfsh/VPL7A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //std::ios::sync_with_stdio(false);
+    //std::cin.tie(0);
+    
+    long long int x{}, y{}, sum{};
+    cin >> x >> y;
+    if (x < y)
+    {	
+	for (int i = x; i <= y; i++)
+	    sum += i;
+    }
+    else if (x == y) cout << x << endl;
+    else if (x > y)
+    {
+	for (int i = y; i <= x; i++)
+	    sum += i;
+    }
+    cout << sum << "\n";
+
+    return 0;
+}
