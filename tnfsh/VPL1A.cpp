28f1ae5f724d8b875818e81e9cd88fd3e6c60582
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:07:57 +0800

    Create VPL1A.cpp

---
+++ tnfsh/VPL1A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int a{}, b{};
+    cin >> a >> b;
+    int ans = (a + b) / 2;
+    cout << ans << "\n";
+    return 0;
+}
