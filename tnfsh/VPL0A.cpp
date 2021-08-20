6e5da5d3f4f8a06fe608f3be9fa93dde0e47b47c
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:04:44 +0800

    Create VPL0A.cpp

---
+++ tnfsh/VPL0A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "I asked my wife, Where do you want to go on our anniversary?" << endl;
+    cout << "She said, Somewhere I have never been!" << endl;
+    cout << "I told her, How about the kitchen?" << endl;
+    return 0;
+}
