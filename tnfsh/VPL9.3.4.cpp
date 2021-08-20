5b14b60bb05b6be209dba85b62d3fa5ce174a620
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:43:06 +0800

    Create VPL9.3.4.cpp

---
+++ tnfsh/VPL9.3.4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+//#include <string> 
+//#include <sstream>
+//#include <iomanip>
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//#include <cmath>
+//#include <random>
+//#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    //std::ios::sync_with_stdio(false);
+    //std::cin.tie(0);
+    //加速cin cout用
+    // fixed << setprecision(n) << variable
+    //取小數點用
+    
+    int n{};
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+	for (int j = 0; j <= i; j++)
+	    cout << i+1;
+	cout << endl;
+    }
+	 
+    return 0;
+}
