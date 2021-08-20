e97f99745cf426ec5c0706afec4f4a692a9d2dab
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:37:09 +0800

    Create VPL9B.cpp

---
+++ tnfsh/VPL9B.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+#include <iomanip>
+//#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    //std::ios::sync_with_stdio(false);
+    //std::cin.tie(0);
+    
+    int n{}, temp{};
+    bool is_87 = false;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+	cin >> temp; 
+	if (temp == 87) { is_87 = true; break; }
+    }
+    if (is_87 == true) cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}
