d745b097bdfba64e3c90d7518076eb9de9e9fd25
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-20 19:59:40 +0800

    Create VPL13C.cpp

---
+++ tnfsh/VPL13C.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+//#include <string> 
+//#include <string.h> //strlen
+//#include <sstream>
+//#include <iomanip>
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//#include <cmath>
+//#include <random>
+//#include <ctime>
+
+
+
+
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    //加速cin cout用
+    // fixed << setprecision(n) << variable
+    //取小數點用
+    
+    int n{1}, m{};
+    cin >> m;
+    while (n++)
+    {
+        if (n * (n + 1) / 2 >= m)
+	      {
+	          cout << n << endl;
+ 	          break;
+	      }
+    }
+
+    return 0;
+}
