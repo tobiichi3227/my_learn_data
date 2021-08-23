a6dcd99e825b469aa9ada52b2b2c8e96995aac20
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-08-23 17:58:02 +0800

    Create VPL15.2.cpp

---
+++ tnfsh/VPL15.2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+//#include <string>
+//#include <cstring> //strlen
+//#include <sstream>
+//#include <iomanip> //setprecision
+//#include <vector>
+//#include <map>
+//#include <algorithm>
+//#include <cmath>
+//#include <random>
+//#include <ctime>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cctype> isdigit,isupper,islower
+
+using namespace std;
+
+//user function 開始
+int f(int n)
+{
+    if (n > 0 and n % 2 == 1) return (n + f(n - 1));
+    else if (n > 0 and n % 2 == 0) return (-n + f(n - 1));
+    else if (n < 0) return 0;
+}
+//user function 結尾
+
+int main()
+{
+    //std::ios::sync_with_stdio(false); //加速cin cout用
+    //std::cin.tie(0); //加速cin cout用
+    //fixed << setprecision(n) << variable 取小數點用
+    //static_cast<new-type>(expression) 強制型別轉換
+    //int isdigit(int x) return 0 or int 判斷是否為0 - 9
+    //max_element(begin, end) return iterator
+
+    //主要程序碼
+    int n;
+    cin >> n;
+    cout << f(n) << endl;
+
+    //程式碼結尾
+    return 0;
+}
