1da9e20b06a833fa3a92da3da6fa01bb65352365
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 21:43:02 +0800

    Update VPL14B.cpp

--- tnfsh/VPL14B.cpp
+++ tnfsh/VPL14B.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string> 
+#include <string>
 //#include <string.h> //strlen
 //#include <sstream>
 //#include <iomanip>
@@ -10,9 +10,6 @@
 //#include <random>
 //#include <ctime>
 
-
-
-
 using namespace std;
 
 int main()
@@ -22,13 +19,23 @@ int main()
     //加速cin cout用
     // fixed << setprecision(n) << variable
     //取小數點用
-    
-    string number{};
+
+    string number{}; //比較好的解 不太會受到int上限的影響
     int sum{};
     cin >> number;
     for (int i = 0; i < number.length(); i++)
-	sum += number[i] - '0';
+        sum += number[i] - '0';
     cout << sum << endl;
-
+    /*
+    標準解 原理: 一個數的個位數對10取餘數就是個位數本身,接著再將這個以計算的個位數消掉
+    將這個數/=10,就可以將以計算的數削掉,因為long long int,所以不會有小數點的產生
+    long long int temp{}, sum{};
+    cin >> temp;
+    while(temp != 0)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    */
     return 0;
 }
