e48f8e8e912d1f10dc40c6a95b5435288b788c7c
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:27:40 +0800

    Update VPL7B.cpp

--- tnfsh/VPL7B.cpp
+++ tnfsh/VPL7B.cpp
@@ -8,7 +8,7 @@ int main()
 {
     //std::ios::sync_with_stdio(false);
     //std::cin.tie(0);
-    
+
     int n{};
     cin >> n;
     cout << factorial(n) << "\n";
@@ -18,9 +18,12 @@ int main()
 
 long long int factorial(int x)
 {
-    if (x == 0) return 1;
-    if (x == 1) return 1;
+    if (x == 0)
+        return 1;
+    if (x == 1)
+        return 1;
     if (x > 1)
-	return x * factorial(x - 1);
-    else return 1;
+        return x * factorial(x - 1);
+    else
+        return 1;
 }
