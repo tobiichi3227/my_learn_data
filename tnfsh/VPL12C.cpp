947a78c9e054be9b8e4a172c9182a0ddab7958c2
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 21:01:49 +0800

    Update VPL12C.cpp

--- tnfsh/VPL12C.cpp
+++ tnfsh/VPL12C.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <string> 
+//#include <string>
 //#include <string.h> //strlen
 //#include <sstream>
 #include <iomanip>
@@ -10,9 +10,6 @@
 //#include <random>
 //#include <ctime>
 
-
-
-
 using namespace std;
 
 int main()
@@ -22,15 +19,15 @@ int main()
     //加速cin cout用
     // fixed << setprecision(n) << variable
     //取小數點用
-    
+
     int n{}, temp{}, temp2{};
     double sum{};
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-	      cin >> temp >> temp2;
-	      sum += static_cast<double>(temp2) - static_cast<double>(temp);
-	      cout << "Inv(" << (i + 1) <<"): " << fixed << setprecision(2) << sum << "\n";
+        cin >> temp >> temp2;
+        sum += static_cast<double>(temp2) - static_cast<double>(temp);
+        cout << "Inv(" << (i + 1) << "): " << fixed << setprecision(2) << sum << "\n";
     }
     return 0;
 }
