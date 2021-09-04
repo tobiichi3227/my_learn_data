9eccf8821ba0465fc35ffa1069e61f98f70dc2ab
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:38:48 +0800

    Update VPL8D.cpp

--- tnfsh/VPL8D.cpp
+++ tnfsh/VPL8D.cpp
@@ -15,7 +15,7 @@ int main()
     int n{}, sum{};
     bool is_prime = true;
     cin >> n;
-    for (int i = 2; i < n / 2; i++)
+    for (int i = 2; i <= n / 2; i++)
     {
         if (n == 2)
             cout << "XD\n";
