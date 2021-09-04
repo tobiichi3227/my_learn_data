ca456dee3ad7e04894ee03ec66e8132f56645714
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 21:00:56 +0800

    Update VPL12A.cpp

--- tnfsh/VPL12A.cpp
+++ tnfsh/VPL12A.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <string> 
+//#include <string>
 //#include <string.h> //strlen
 //#include <sstream>
 //#include <iomanip>
@@ -10,9 +10,6 @@
 //#include <random>
 //#include <ctime>
 
-
-
-
 using namespace std;
 
 int main()
@@ -22,28 +19,28 @@ int main()
     //加速cin cout用
     // fixed << setprecision(n) << variable
     //取小數點用
-    
+
     int n{};
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-	for (int k = 0; k < i; k++)
-	    cout << " ";
-	for (int j = 0; j < 2 * n - 1 - 2 * i; j++)
-	    cout << "*";
-	//for (int k = 0; k < i ; k++)
-	    //cout << " ";
-	cout << "\n"; 
-    }      
+        for (int k = 0; k < i; k++)
+            cout << " ";
+        for (int j = 0; j < 2 * n - 1 - 2 * i; j++)
+            cout << "*";
+        //for (int k = 0; k < i ; k++)
+        //cout << " ";
+        cout << "\n";
+    }
     for (int i = 0; i < n - 1; i++)
     {
-	for (int k = n - 2; k > i; k--)
-	    cout << " ";
-	for (int j = 0; j < 2 * i + 3; j++)
-	    cout << "*";
-	//for (int k = n - 2; k > i; k--)
-	    //cout << " ";
-	cout << endl;
+        for (int k = n - 2; k > i; k--)
+            cout << " ";
+        for (int j = 0; j < 2 * i + 3; j++)
+            cout << "*";
+        //for (int k = n - 2; k > i; k--)
+        //cout << " ";
+        cout << endl;
     }
     return 0;
 }
