864035adfb2beed8118e81dffb3526ec50c382c3
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:23:39 +0800

    Update VPL6B.cpp

--- tnfsh/VPL6B.cpp
+++ tnfsh/VPL6B.cpp
@@ -10,10 +10,10 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int n{}, m{};
     cin >> n >> m;
     for (int i = n; i <= m; i++)
-        cout << i << "C=" << i*(9.0/5.0)+32 << "F" << "\n";
-  
+        cout << i << "C=" << i * (9.0 / 5.0) + 32 << "F"
+             << "\n";
 }
