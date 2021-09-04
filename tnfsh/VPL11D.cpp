639dac8a0ea644c0d03205716d1367562caead7e
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:59:41 +0800

    Update VPL11D.cpp

--- tnfsh/VPL11D.cpp
+++ tnfsh/VPL11D.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <string> 
+//#include <string>
 //#include <string.h> //strlen
 //#include <sstream>
 //#include <iomanip>
@@ -10,9 +10,6 @@
 //#include <random>
 //#include <ctime>
 
-
-
-
 using namespace std;
 
 int main()
@@ -22,23 +19,24 @@ int main()
     //加速cin cout用
     // fixed << setprecision(n) << variable
     //取小數點用
-    
+
     int n{}, x1{}, temp{};
     bool is_87 = false;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-	is_87 = false;
-	cin >> x1;
-	for (int j = 0; j < x1; j++)
-	{
-	    cin >> temp;
-	    if (temp == 87)
-		is_87 = true;
-	}
-	if (is_87 == true)
-	    cout << i+1 << ":YES\n";
-	else cout << i+1 << ":NO\n"; 
+        is_87 = false;
+        cin >> x1;
+        for (int j = 0; j < x1; j++)
+        {
+            cin >> temp;
+            if (temp == 87)
+                is_87 = true;
+        }
+        if (is_87 == true)
+            cout << i + 1 << ":YES\n";
+        else
+            cout << i + 1 << ":NO\n";
     }
     return 0;
 }
