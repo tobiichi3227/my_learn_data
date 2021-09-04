b916153b7bc5efe1724e682f1dca0ef1ef8bc9cb
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:37:22 +0800

    Update VPL8D.cpp

--- tnfsh/VPL8D.cpp
+++ tnfsh/VPL8D.cpp
@@ -11,21 +11,23 @@ int main()
 {
     //std::ios::sync_with_stdio(false);
     //std::cin.tie(0);
-    
+
     int n{}, sum{};
     bool is_prime = true;
     cin >> n;
     for (int i = 2; i < n / 2; i++)
     {
-	if (n == 2) cout << "XD\n";
-	else if (n % i == 0)
-	{
-	    sum += i;
-	    is_prime = false;
-	}
+        if (n == 2)
+            cout << "XD\n";
+        else if (n % i == 0)
+        {
+            sum += i;
+            is_prime = false;
+        }
     }
-    if (is_prime == true) cout << "XD\n";
-    else 
-    	cout << sum << "\n";
+    if (is_prime == true)
+        cout << "XD\n";
+    else
+        cout << sum << "\n";
     return 0;
 }
