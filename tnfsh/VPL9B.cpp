49d74dab174d36ccfedb262167e284e2ccd63704
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:41:33 +0800

    Update VPL9B.cpp

--- tnfsh/VPL9B.cpp
+++ tnfsh/VPL9B.cpp
@@ -11,16 +11,22 @@ int main()
 {
     //std::ios::sync_with_stdio(false);
     //std::cin.tie(0);
-    
+
     int n{}, temp{};
     bool is_87 = false;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-	cin >> temp; 
-	if (temp == 87) { is_87 = true; break; }
+        cin >> temp;
+        if (temp == 87)
+        {
+            is_87 = true;
+            break;
+        }
     }
-    if (is_87 == true) cout << "YES\n";
-    else cout << "NO\n";
+    if (is_87 == true)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
     return 0;
 }
