540d71827d8706b0e8d6d35bb8adb064931f5e60
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 19:47:47 +0800

    Update VPL2A.cpp

--- tnfsh/VPL2A.cpp
+++ tnfsh/VPL2A.cpp
@@ -9,10 +9,9 @@ int main()
     double a{}, b{}, c{};
     while (cin >> a >> b >> c)
     {
-         double S = (a + b + c) / 2;
-         double area = sqrt(S * (S-a) * (S-b) * (S-c));
-         cout << fixed << setprecision(2) <<area << endl;
+        double S = (a + b + c) / 2;
+        double area = sqrt(S * (S - a) * (S - b) * (S - c));
+        cout << fixed << setprecision(2) << area << endl;
     }
     return 0;
-    
 }
