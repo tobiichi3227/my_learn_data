b3e9eafe08057352b1a7a324a9dadaa47f1da0de
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:07:07 +0800

    Update VPL4C.cpp

--- tnfsh/VPL4C.cpp
+++ tnfsh/VPL4C.cpp
@@ -6,16 +6,15 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int a{};
     while (cin >> a)
     {
-	if (a % 2 == 0)
-            cout << (a/2) << "\n";
-        else 
+        if (a % 2 == 0)
+            cout << (a / 2) << "\n";
+        else
             cout << (3 * a + 1) << "\n";
     }
- 
+
     return 0;
-  
 }
