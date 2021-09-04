0e778057e83a72363bdafd9c801fe3cfb0d7d57f
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:42:39 +0800

    Update VPL9C.cpp

--- tnfsh/VPL9C.cpp
+++ tnfsh/VPL9C.cpp
@@ -11,10 +11,10 @@ int main()
 {
     //std::ios::sync_with_stdio(false);
     //std::cin.tie(0);
-    
+
     int r{}, m{}, n{}, j{0};
     cin >> r >> m >> n;
-    if (m > r) 
+    if (m > r)
     {
         cout << "Bumbler government is not a necessary evil. It is just evil.";
         return 0;
@@ -22,20 +22,18 @@ int main()
     cout << "S";
     for (int i = 1; i < r; i++)
     {
-	//j = 0;
-	if (i % m == 0) 
-	{
-	    cout << "T";
-	    j++;
-	    if (j % n == 0) 
-	    {
-	        cout << "X";
-	        j = 0;
-		i += m;
-	    }       
-	}
-	
-	
+        //j = 0;
+        if (i % m == 0)
+        {
+            cout << "T";
+            j++;
+            if (j % n == 0)
+            {
+                cout << "X";
+                j = 0;
+                i += m;
+            }
+        }
     }
 
     return 0;
