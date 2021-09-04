dc7cad62f843467f73993fe228cabc1f270584d8
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 19:48:35 +0800

    Update VPL2B.cpp

--- tnfsh/VPL2B.cpp
+++ tnfsh/VPL2B.cpp
@@ -9,19 +9,18 @@ int main()
     int a{}, b{}, c{};
     while (cin >> a >> b >> c)
     {
-         double x1 = (-b + sqrt(b*b-4*a*c))/2*a;
-         double x2 = (-b - sqrt(b*b-4*a*c))/2*a;
-         if (x1 > x2)
-         {
-             cout << fixed << setprecision(2) << x1 << "\n";
-             cout << fixed << setprecision(2) << x2 << "\n";
-         }
-         else 
-         {
-             cout << fixed << setprecision(2) << x2 << "\n";
-             cout << fixed << setprecision(2) << x1 << "\n";
-         }
+        double x1 = (-b + sqrt(b * b - 4 * a * c)) / 2 * a;
+        double x2 = (-b - sqrt(b * b - 4 * a * c)) / 2 * a;
+        if (x1 > x2)
+        {
+            cout << fixed << setprecision(2) << x1 << "\n";
+            cout << fixed << setprecision(2) << x2 << "\n";
+        }
+        else
+        {
+            cout << fixed << setprecision(2) << x2 << "\n";
+            cout << fixed << setprecision(2) << x1 << "\n";
+        }
     }
     return 0;
-    
 }
