fcbbc9ccf6aa70d08aa4ac5ccc42f3f8395a23e7
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:27:05 +0800

    Update VPL7A.cpp

--- tnfsh/VPL7A.cpp
+++ tnfsh/VPL7A.cpp
@@ -6,19 +6,20 @@ int main()
 {
     //std::ios::sync_with_stdio(false);
     //std::cin.tie(0);
-    
+
     long long int x{}, y{}, sum{};
     cin >> x >> y;
     if (x < y)
-    {	
-	for (int i = x; i <= y; i++)
-	    sum += i;
+    {
+        for (int i = x; i <= y; i++)
+            sum += i;
     }
-    else if (x == y) cout << x << endl;
+    else if (x == y)
+        cout << x << endl;
     else if (x > y)
     {
-	for (int i = y; i <= x; i++)
-	    sum += i;
+        for (int i = y; i <= x; i++)
+            sum += i;
     }
     cout << sum << "\n";
 
