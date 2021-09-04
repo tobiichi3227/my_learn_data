821c80dcf65a42ee759f66c5beaca2728eedc4b8
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:32:42 +0800

    Update VPL8A.cpp

--- tnfsh/VPL8A.cpp
+++ tnfsh/VPL8A.cpp
@@ -11,17 +11,22 @@ int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
-    
+
     int n{}, temp{};
     cin >> n;
     bool all_odd = true;
     for (int i = 0; i < n; i++)
     {
-	cin >> temp;
-	if (temp % 2 == 0) all_odd = false;
+        cin >> temp;
+        if ((temp & 1) == 0) // x & 1 == -> x % 2 == 0 &運算階級<恆等於運算階級 所以要加括號
+            all_odd = false;
     }
-    if (all_odd == true) cout << "Very Good!!!" << "\n";
-    else cout << "Oh, No!!!" << "\n";
+    if (all_odd == true)
+        cout << "Very Good!!!"
+             << "\n";
+    else
+        cout << "Oh, No!!!"
+             << "\n";
 
     return 0;
 }
