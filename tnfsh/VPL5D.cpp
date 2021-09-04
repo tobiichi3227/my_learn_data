510c851cd33807dfd976705f9db0bd39c498200a
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-04 20:20:55 +0800

    Update VPL5D.cpp

--- tnfsh/VPL5D.cpp
+++ tnfsh/VPL5D.cpp
@@ -6,17 +6,39 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int b1{}, b2{}, p1{}, p2{};
     cin >> b1 >> b2 >> p1 >> p2;
-    
-    if ((b1 + b2) > 17 and (p1 + p2) > 17) { cout << "Draw." << endl; return 0; }
-    else if ((b1 + b2) > 17) { cout << "Player wins." << endl; return 0; }
-    else if ((b1 + b2) == (p1 + p2)) { cout << "Banker wins." << endl; return 0; }
-    else if ((p1 + p2) > 17) { cout << "Banker wins." << endl; return 0; }
-    else if ((b1 + b2) > (p1 + p2)) { cout << "Banker wins." << endl; return 0; }
-    else if ((b1 + b2) < (p1 + p2)) { cout << "Player wins." << endl; return 0; }
- 
+
+    if ((b1 + b2) > 17 and (p1 + p2) > 17)
+    {
+        cout << "Draw." << endl;
+        return 0;
+    }
+    else if ((b1 + b2) > 17)
+    {
+        cout << "Player wins." << endl;
+        return 0;
+    }
+    else if ((b1 + b2) == (p1 + p2))
+    {
+        cout << "Banker wins." << endl;
+        return 0;
+    }
+    else if ((p1 + p2) > 17)
+    {
+        cout << "Banker wins." << endl;
+        return 0;
+    }
+    else if ((b1 + b2) > (p1 + p2))
+    {
+        cout << "Banker wins." << endl;
+        return 0;
+    }
+    else if ((b1 + b2) < (p1 + p2))
+    {
+        cout << "Player wins." << endl;
+        return 0;
+    }
     return 0;
-  
 }
