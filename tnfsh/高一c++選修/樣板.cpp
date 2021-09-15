ba10bf155e38343dd66322d48c80419398f82dbb
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-09-15 20:40:37 +0800

    Create 樣板.cpp

---
+++ tnfsh/高一c++選修/樣板.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+//https://hackmd.io/@sa072686/APCS_HARD/https%3A%2F%2Fhackmd.io%2Fs%2FSknKMqIQ7 //南一中APCS2018年
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false), std::cin.tie(nullptr); //加速cin, cout
+    cout << fixed << setprecision(n) << variable //小數點控制
+    EOF是ctrl+z, 如果要輸入到EOF,寫法如下
+    while (cin >> variable)
+    {
+        do_code
+    }
+    static_cast<new-type> (expression) //比較好的強制類型轉換
+    if ((x&1) == 0) return isn't_prime else return is_prime //奇數判斷
+    其他的技巧可以在2018APCS中學到
+    
+    
