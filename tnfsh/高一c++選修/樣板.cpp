3b3d3183174c841bbc90b883fa45b2b25833aa6d
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-11-19 08:16:44 +0800

    Update 樣板.cpp

--- tnfsh/高一c++選修/樣板.cpp
+++ tnfsh/高一c++選修/樣板.cpp
@@ -2,6 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/stack:200000000")
+
 #define ll long long
 #define ld long double
 #define ar array
