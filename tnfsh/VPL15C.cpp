8d3eeb3cbafe5c868a000fe8932eac2501314b56
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-11-28 20:56:30 +0800

    Update VPL15C.cpp

--- tnfsh/VPL15C.cpp
+++ tnfsh/VPL15C.cpp
@@ -1,42 +1,120 @@
-#include <iostream>
-//#include <string> 
-//#include <cstring> //strlen
-//#include <sstream>
-//#include <iomanip> //setprecision
-#include <vector>
-//#include <map>
-#include <algorithm> 
-//#include <cmath> 
-//#include <random>
-//#include <ctime>
-//#include <cstdio>
-//#include <cstdlib>
-//#include <cctype> isdigit,isupper,islower
-
+#include <bits/stdc++.h>
 using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+#define fastio ios::sync_with_stdio(false), cin.tie(nullptr)
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) static_cast<int>((x).size())
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...)       \
+    F_ORC(__VA_ARGS__) \
+    (__VA_ARGS__)
+#define EACH(x, a) for (auto &x : a)
+
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &arr)
+{
+    for (auto it = arr.cbegin(); it != arr.cend(); ++it)
+        out << *it << " ";
+    out << "\n";
+    return out;
+}
+
+template <typename S, typename T>
+ostream &operator<<(ostream &out, const map<S, T> &mp)
+{
+    for (auto it = mp.cbegin(); it != mp.cend(); ++it)
+        out << it->first << " " << it->second << "\n";
+    return out;
+}
+
+template <typename S, typename T>
+ostream &operator<<(ostream &out, const pair<S, T> &pr)
+{
+    for (auto it = pr.cbegin(); it != pr.cend(); ++it)
+        out << it->fist << " " << it->second << "\n";
+    return out;
+}
+
+inline const int read()
+{
+    int ret{0}, f{1};
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        ret = (ret << 3) + (ret << 1) + (ch ^ 48), ch = getchar();
+    return ret * f;
+}
+
+inline const ll llread()
+{
+    ll ret{0}, f{1};
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = (ll)-1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        ret = ret * (ll)10 + (ll)(ch ^ 48), ch = getchar();
+    return (ll)ret * f;
+}
+
+inline void print(int x)
+{
+    static int sta[35];
+    int top{0};
+    do
+    {
+        sta[top++] = x % 10, x /= 10;
+    } while (x);
+    while (top)
+        putchar(sta[--top] + 48);
+}
+
+int n{}, temp{}, sum{}, score_min{INT_MAX}, score_max{0};
 
 int main()
 {
-    //std::ios::sync_with_stdio(false); //加速cin cout用
-    //std::cin.tie(0); //加速cin cout用
-    //fixed << setprecision(n) << variable 取小數點用
-    //static_cast<new-type>(expression) 強制型別轉換
-    //int isdigit(int x) return 0 or int 判斷是否為0 - 9
-    //max_element(begin, end) return iterator
-    
-    //主要程序碼
-    int n{}, total{}, average{}, max{}, min{};
-    cin >> n;
-    vector<int> temp(n);
-    for (int i = 0; i < n; i++)
-    	cin >> temp[i];
-    for (auto &x : temp) total += x;
-    average = total / n;
-    max = *max_element(temp.begin(), temp.end());
-    min = *min_element(temp.begin(), temp.end());
-    cout << total << "\n" << average << "\n" << max << "\n" << min << "\n";
-    
-    //程式碼結尾
+    fastio;
+
+    while (cin >> n) //多筆測資
+    {
+        FOR(n)
+        {
+            temp = read();
+            sum += temp;
+            if (temp < score_min)
+                score_min = temp;
+            if (temp > score_max)
+                score_max = temp;
+        }
+        cout << sum << "\n"
+             << (sum / n) << "\n"
+             << score_max << "\n"
+             << score_min << endl;
+    }
 
     return 0;
 }
