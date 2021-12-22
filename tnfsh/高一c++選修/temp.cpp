3b5da1dca8f48e7ccf6c0bed397e9433cd4a7ae8
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2021-12-22 22:42:39 +0800

    Add files via upload

---
+++ tnfsh/高一c++選修/temp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+#define int long long
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &x : arr)
+#define meminit(arr, element) memset((arr), (element), sizeof((arr)))
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+
+using pii = std::pair<int, int>;
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+signed main()
+{
+    miyuki_is_my_wife;
+
+    return 0;
+}
