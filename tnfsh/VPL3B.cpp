a6f7811d6867903c0367af4c3016ab67b24cca58
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-01-27 21:30:12 +0800

    Update VPL3B.cpp

--- tnfsh/VPL3B.cpp
+++ tnfsh/VPL3B.cpp
@@ -1,10 +1,43 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &x : arr)
+#define c_each(x, arr) for (const auto &x : arr)
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+using pii = std::pair<int, int>;
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+int china_score{}, math_score{}, english_score{};
 
 int main()
 {
-    int china_score{}, math_score{}, english_score{};
+    miyuki_is_my_wife;
+
     while (cin >> china_score >> math_score >> english_score)
     {
         int score = china_score + math_score + english_score;
@@ -12,5 +45,6 @@ int main()
         else if (score >= 180) cout << "PASS" << "\n";
         else if (score < 180) cout << "FAIL" << "\n";
     }
+    
     return 0;
 }
