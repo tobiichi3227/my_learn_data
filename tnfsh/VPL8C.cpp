79a89bb58b027e69091405e581bdfece8e843dae
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-01-27 21:59:58 +0800

    Update VPL8C.cpp

--- tnfsh/VPL8C.cpp
+++ tnfsh/VPL8C.cpp
@@ -1,29 +1,60 @@
-#include <iostream>
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-//#include <iomanip>
-//#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &x : arr)
+#define c_each(x, arr) for (const auto &x : arr)
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+using pii = std::pair<int, int>;
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+int n{}, sum{};
 
 int main()
 {
-    //std::ios::sync_with_stdio(false);
-    //std::cin.tie(0);
-    
-    int n{}, sum{};
-    bool is_prime = true;
+    miyuki_is_my_wife;
+
+	bool is_prime = true;
     cin >> n;
     for (int i = 2; i <= n / 2; i++)
     {
-	if (n == 2) cout << "prime" << endl;
-	else if (n % i == 0)
-	{
-	    cout << i << "\n";
-	    is_prime = false;
+		if (n == 2) 
+			cout << "prime" << '\n';
+		else if (n % i == 0)
+		{
+			cout << i << "\n";
+			is_prime = false;
+		}
 	}
-    }
-    if (is_prime == true) cout << "prime" << endl;
+    if (is_prime) 
+		cout << "prime" << '\n';
+	
     return 0;
 }
+
+
+
