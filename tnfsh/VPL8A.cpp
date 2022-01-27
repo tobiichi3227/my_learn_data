6c247b21e376acc0a3f9299ea1b9b0da0e821f69
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-01-27 21:57:12 +0800

    Update VPL8A.cpp

--- tnfsh/VPL8A.cpp
+++ tnfsh/VPL8A.cpp
@@ -1,32 +1,56 @@
-#include <iostream>
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-//#include <iomanip>
-//#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &x : arr)
+#define c_each(x, arr) for (const auto &x : arr)
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+using pii = std::pair<int, int>;
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+int n{}, temp{};
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
+    miyuki_is_my_wife;
 
-    int n{}, temp{};
     cin >> n;
     bool all_odd = true;
     for (int i = 0; i < n; i++)
     {
         cin >> temp;
-        if ((temp & 1) == 0) // x & 1 == -> x % 2 == 0 &運算階級<恆等於運算階級 所以要加括號
+        if (!(temp & 1)) // x & 1 == -> x % 2 == 0 &運算階級<恆等於運算階級 所以要加括號
             all_odd = false;
     }
     if (all_odd == true)
-        cout << "Very Good!!!"
-             << "\n";
+        cout << "Very Good!!!" << "\n";
     else
-        cout << "Oh, No!!!"
-             << "\n";
-
+        cout << "Oh, No!!!" << "\n";
+    
     return 0;
 }
+
