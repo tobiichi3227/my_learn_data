19c883269734b6698b41bf4e1c09adf30776a2d6
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-01-27 21:50:00 +0800

    Update VPL7A.cpp

--- tnfsh/VPL7A.cpp
+++ tnfsh/VPL7A.cpp
@@ -1,27 +1,54 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &x : arr)
+#define c_each(x, arr) for (const auto &x : arr)
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+using pii = std::pair<int, int>;
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+ll x{}, y{};
 
 int main()
 {
-    //std::ios::sync_with_stdio(false);
-    //std::cin.tie(0);
+    miyuki_is_my_wife;
 
-    long long int x{}, y{}, sum{};
     cin >> x >> y;
-    if (x < y)
+    auto n_sum = [](const ll& x) -> ll{
+        return ((x * (x + 1)) >> 1);
+    };
+    if (x > y)
     {
-        for (int i = x; i <= y; i++)
-            sum += i;
+        swap(x, y);
     }
-    else if (x == y)
-        cout << x << endl;
-    else if (x > y)
-    {
-        for (int i = y; i <= x; i++)
-            sum += i;
-    }
-    cout << sum << "\n";
-
+    cout << n_sum(y) - n_sum(x - 1) << '\n';
+    
     return 0;
 }
+
+
