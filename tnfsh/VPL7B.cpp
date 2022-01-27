097898deb32ff67bfa1e0d2e3d873296f706c624
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-01-27 21:52:21 +0800

    Update VPL7B.cpp

--- tnfsh/VPL7B.cpp
+++ tnfsh/VPL7B.cpp
@@ -1,29 +1,54 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-long long int factorial(int);
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &x : arr)
+#define c_each(x, arr) for (const auto &x : arr)
+#define rep(i, j, k) for (int i = j; i <= k; ++i)
+using pii = std::pair<int, int>;
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+int n{};
+ll dp[25];
 
 int main()
 {
-    //std::ios::sync_with_stdio(false);
-    //std::cin.tie(0);
+    miyuki_is_my_wife;
 
-    int n{};
+    dp[0] = 1;
+    rep(i, 1, 24)
+    {
+        dp[i] = dp[i - 1] * i;
+    }
     cin >> n;
-    cout << factorial(n) << "\n";
-
+    cout << dp[n] << '\n';
+    
     return 0;
 }
 
-long long int factorial(int x)
-{
-    if (x == 0)
-        return 1;
-    if (x == 1)
-        return 1;
-    if (x > 1)
-        return x * factorial(x - 1);
-    else
-        return 1;
-}
+
+
