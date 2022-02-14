a46c5082e2b514b4ad918d278d5903ed78db8ef7
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-02-14 21:14:17 +0800

    Update VPL9D.cpp

--- tnfsh/VPL9D.cpp
+++ tnfsh/VPL9D.cpp
@@ -1,18 +1,42 @@
-#include <iostream>
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-//#include <iomanip>
-//#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+using pii = std::pair<int, int>;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &(x) : (arr))
+#define c_each(x, arr) for (const auto &(x) : (arr))
+#define rep(i, j, k) for (int (i) = (j); (i) <= (k); ++(i))
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
 
 int main()
 {
-    //std::ios::sync_with_stdio(false);
-    //std::cin.tie(0);
-    
-    long long fib[75];
+    miyuki_is_my_wife;
+
+	long long fib[75];
     int n, i, j{0};
     fib[0] = 0;
     fib[1] = 1;
@@ -22,16 +46,14 @@ int main()
     cin >> n;
     while(j<=n)
     {
-        if (n == 0) 
-	{
-	    cout << "0" << " ";
-	    break;
-	}
-	cout << fib[j] << " ";
-	j++;
+		if (n == 0) 
+		{
+			cout << "0" << " ";
+			break;
+		}
+		cout << fib[j] << " ";
+		j++;
     }
-
-    
-
+	
     return 0;
 }
