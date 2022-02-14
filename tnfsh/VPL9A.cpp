1b084c97576e52509dc924ccce4fe8347f5744bd
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-02-14 21:04:44 +0800

    Update VPL9A.cpp

--- tnfsh/VPL9A.cpp
+++ tnfsh/VPL9A.cpp
@@ -1,25 +1,51 @@
-#include <iostream>
-//#include <string>
-//#include <vector>
-//#include <algorithm>
-#include <iomanip>
-//#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma pack(1)
+
+using ll = long long;
+using pii = std::pair<int, int>;
+#define pb push_back
+#define ss second
+#define ff first
+#define dd cout << '\n';
+#define all(container) (container).begin(), (container).end()
+#define sz(container) static_cast<int>((container).size())
+#define each(x, arr) for (auto &(x) : (arr))
+#define c_each(x, arr) for (const auto &(x) : (arr))
+#define rep(i, j, k) for (int (i) = (j); (i) <= (k); ++(i))
+
+#if __cplusplus >= 201402L
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+#endif
+
+#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+
+int n{}, sum{}, temp{};
 
 int main()
 {
-    //std::ios::sync_with_stdio(false);
-    //std::cin.tie(0);
-    
-    int n{}, sum{}, temp{};
-    cin >> n;
+    miyuki_is_my_wife;
+
+	cin >> n;
     for (int i = 0; i < n; i++)
     {
-	cin >> temp; 
-	sum += temp;
+		cin >> temp; 
+		sum += temp;
     }
-    double average = double(sum) / n;
+    double average = double(sum) / double(n);
     cout << fixed << setprecision(2) << average << "\n";
+	
     return 0;
 }
