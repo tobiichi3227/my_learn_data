1770b4ef2fe96aec14d19e865b52157cd52985c9
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-05-19 14:39:50 +0800

    new version
    使用可變參數模板實現ccin

--- tnfsh/高一c++選修/新模板.cpp
+++ tnfsh/高一c++選修/新模板.cpp
@@ -1,39 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// #include<bits/extc++.h>
+// using namespace __gnu_pbds;
+
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
 #pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
 #pragma pack(1)
 
 using ll = long long;
 using pii = std::pair<int, int>;
+using pll = std::pair<ll, ll>;
 #define pb push_back
+#define eb emplace_back
 #define ss second
 #define ff first
 #define dd cout << '\n';
 #define all(container) (container).begin(), (container).end()
 #define sz(container) static_cast<int>((container).size())
-#define each(i, arr) for (auto &x : arr)
-#define c_each(i, arr) for (const auto &i : arr)
-#define rep(i, j, k) for (i = j; i <= k; ++i)
-#define print(str) cout << (str)
+#define each(x, arr) for (auto &(x) : (arr))
+#define c_each(x, arr) for (const auto &(x) : (arr))
+#define rep(i, j, k) for (int (i) = (j); (i) <= (k); ++(i))
+#define INF 0x3f
 
-void solve() noexcept
-{
-    
-}
+#if __cplusplus > 201703L
+template<typename... T>
+void ccin   (T&... args) { ((cin >> args), ...); }
+template<typename... T>
+void ccout  (T&... args) { ((cout << args << " "), ...); }
+template<typename... T>
+void ccoutl (T&... args) { ((cout << args << " "), ...); cout << '\n'; }
+#endif
 
-#define miyuki_is_my_wife ios::sync_with_stdio(false), cin.tie(nullptr)
+#define miyuki_is_my_wife_and_sister ios::sync_with_stdio(false), cin.tie(nullptr)
 
 int main()
 {
-    miyuki_is_my_wife;
+    miyuki_is_my_wife_and_sister;
 
-    int tc{}; //int tc{1};
-    cin >> tc;
-    while (tc--) {
-        solve();
-    }
-    
     return 0;
 }
