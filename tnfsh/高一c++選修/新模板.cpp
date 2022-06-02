7d0a363e6206e41b6b4926709473c85adac78e29
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-06-02 09:09:45 +0800

    using rust type name

--- tnfsh/高一c++選修/新模板.cpp
+++ tnfsh/高一c++選修/新模板.cpp
@@ -8,9 +8,12 @@ using namespace std;
 #pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
 #pragma pack(1)
 
-using ll = long long;
-using pii = std::pair<int, int>;
-using pll = std::pair<ll, ll>;
+using i32 = int;
+using i64 = long long;
+using f32 = double;
+using f64 = long double;
+using pii = std::pair<i32, i32>;
+using pll = std::pair<i64, i64>;
 #define pb push_back
 #define eb emplace_back
 #define ss second
@@ -20,21 +23,19 @@ using pll = std::pair<ll, ll>;
 #define sz(container) static_cast<int>((container).size())
 #define each(x, arr) for (auto &(x) : (arr))
 #define c_each(x, arr) for (const auto &(x) : (arr))
-#define rep(i, j, k) for (int (i) = (j); (i) <= (k); ++(i))
+#define rep(i, j, k) for (i32 (i) = (j); (i) <= (k); ++(i))
 #define INF 0x3f
 
-// #if __cplusplus > 201703L
 template<typename... T>
 void ccin   (T&... args) { ((cin >> args), ...); }
 template<typename... T>
 void ccout  (T&&... args) { ((cout << args << " "), ...); }
 template<typename... T>
 void ccoutl (T&&... args) { ((cout << args << " "), ...); cout << '\n'; }
-// #endif
 
 #define miyuki_is_my_wife_and_sister ios::sync_with_stdio(false), cin.tie(nullptr)
 
-int main()
+i32 main()
 {
     miyuki_is_my_wife_and_sister;
 
