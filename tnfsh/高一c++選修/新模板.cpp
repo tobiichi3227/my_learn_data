660ab3318836232bfe50f4f432fcc1d5b6f2f584
tobiichi3227 <86729076+tobiichi3227@users.noreply.github.com>
2022-06-06 16:01:20 +0800

    upgrade
    參考LittleCube加入operator overloading

--- tnfsh/高一c++選修/新模板.cpp
+++ tnfsh/高一c++選修/新模板.cpp
@@ -27,12 +27,24 @@ using order_set      = __gnu_pbds::tree<i32, __gnu_pbds::null_type, less<i32>, _
 #define INF 0x3f
 
 // #if __cplusplus > 201703L
-template<typename... T>
-inline void ccin   (T&... args)  { ((cin >> args), ...); }
-template<typename... T>
-inline void ccout  (T&&... args)  { ((cout << args << " "), ...); }
-template<typename... T>
-inline void ccoutl (T&&... args) { ((cout << args << " "), ...); cout << '\n'; }
+template <typename T, typename U>
+std::istream& operator>>(std::istream &is, std::pair<T, U> &val) {
+    is >> val.first >> val.second;
+    return is;
+}
+template <typename T>
+std::istream& operator>>(std::istream &is, std::vector<T> &arr) {
+    for (T &it : arr) {
+        is >> it;
+    }
+    return is;
+}
+template <typename... T>
+inline void ccin   (T&... args )  { ((std::cin >> args), ...); }
+template <typename... T>
+inline void ccout  (T&&... args)  { ((std::cout << args << " "), ...); }
+template <typename... T>
+inline void ccoutl (T&&... args)  { ((std::cout << args << " "), ...); std::cout << '\n'; }
 // #endif
 
 #define miyuki_is_my_wife_and_sister ios::sync_with_stdio(false), cin.tie(nullptr)
